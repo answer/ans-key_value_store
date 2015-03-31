54e00101c8d26104bcb1f830fa67171b8d26c168
sakai shunsuke <sakai@ans-web.co.jp>
2015-03-31 18:28:24 +0900

    schema にてコメントを付けられるように修正

--- test/ans/key_value_store/cast_test.rb
+++ test/ans/key_value_store/cast_test.rb
@@ -23,78 +23,66 @@ module Ans
         Setting.data.instance_variable_set :@stored_data, nil
       end
       it "copy_right" do
-        Setting.data.copy_right = "answer"
-        assert{Setting.copy_right.nil?}
-        Setting.data.save!
+        Setting.data.update! copy_right: "answer"
         assert{Setting.find_by(key: "copy_right").value == "answer"}
         Setting.data.reload
         assert{Setting.copy_right == "answer"}
       end
       it "retry_limit" do
-        Setting.data.retry_limit = "3"
-        assert{Setting.retry_limit.nil?}
-        Setting.data.save!
+        Setting.data.update! retry_limit: "3"
         assert{Setting.find_by(key: "retry_limit").value == "3"}
         Setting.data.reload
         assert{Setting.retry_limit == 3}
       end
       it "consumption_tax_rate" do
-        Setting.data.consumption_tax_rate = "0.8"
-        assert{Setting.consumption_tax_rate.nil?}
-        Setting.data.save!
+        Setting.data.update! consumption_tax_rate: "0.8"
         assert{Setting.find_by(key: "consumption_tax_rate").value == "0.8"}
         Setting.data.reload
         assert{Setting.consumption_tax_rate == BigDecimal.new("0.8")}
       end
       it "start_on" do
-        Setting.data.start_on = "2015/01/01"
-        assert{Setting.start_on.nil?}
-        Setting.data.save!
+        Setting.data.update! start_on: "2015/01/01"
         assert{Setting.find_by(key: "start_on").value == "2015-01-01"}
         Setting.data.reload
         assert{Setting.start_on == Date.parse("2015/01/01")}
       end
-      describe "utc 設定" do
-        before do
-          ActiveRecord::Base.default_timezone = :utc
-        end
-        it "start_at" do
-          Setting.data.start_at = "2015/01/01 10:00"
-          assert{Setting.start_at.nil?}
-          Setting.data.save!
-          assert{Setting.find_by(key: "start_at").value == Time.utc(2015,1,1,10,0,0).to_s}
-          Setting.data.reload
-          assert{Setting.start_at == Time.utc(2015,1,1,10,0,0)}
-        end
-        it "start" do
-          Setting.data.start = "10:00"
-          assert{Setting.start.nil?}
-          Setting.data.save!
-          assert{Setting.find_by(key: "start").value == Time.utc(2000,1,1,10,0,0).to_s}
-          Setting.data.reload
-          assert{Setting.start == Time.utc(2000,1,1,10,0,0)}
-        end
+    end
+    describe "文字列での設定とキャスト、永続化からの復元: utc 設定" do
+      before do
+        ActiveRecord::Base.default_timezone = :utc
+        Setting.data.instance_variable_set :@data, nil
+        Setting.data.instance_variable_set :@stored_data, nil
       end
-      describe "local 設定" do
-        before do
-          ActiveRecord::Base.default_timezone = :local
-        end
-        it "start_at" do
-          Setting.data.start_at = "2015/01/01 10:00"
-          assert{Setting.start_at.nil?}
-          Setting.data.save!
-          assert{Setting.find_by(key: "start_at").value == Time.local(2015,1,1,10,0,0).to_s}
-          Setting.data.reload
-          assert{Setting.start_at == Time.local(2015,1,1,10,0,0)}
-        end
-        it "start" do
-          Setting.data.start = "10:00"
-          assert{Setting.start.nil?}
-          Setting.data.save!
-          assert{Setting.find_by(key: "start").value == Time.local(2000,1,1,10,0,0).to_s}
-          Setting.data.reload
-          assert{Setting.start == Time.local(2000,1,1,10,0,0)}
-        end
+      it "start_at" do
+        Setting.data.update! start_at: "2015/01/01 10:00"
+        assert{Setting.find_by(key: "start_at").value == Time.utc(2015,1,1,10,0,0).to_s}
+        Setting.data.reload
+        assert{Setting.start_at == Time.utc(2015,1,1,10,0,0)}
+      end
+      it "start" do
+        Setting.data.update! start: "10:00"
+        assert{Setting.find_by(key: "start").value == Time.utc(2000,1,1,10,0,0).to_s}
+        Setting.data.reload
+        assert{Setting.start == Time.utc(2000,1,1,10,0,0)}
+      end
+    end
+    describe "文字列での設定とキャスト、永続化からの復元: local 設定" do
+      before do
+        ActiveRecord::Base.default_timezone = :local
+        Setting.data.instance_variable_set :@data, nil
+        Setting.data.instance_variable_set :@stored_data, nil
+      end
+      it "start_at" do
+        Setting.data.update! start_at: "2015/01/01 10:00"
+        assert{Setting.find_by(key: "start_at").value == Time.local(2015,1,1,10,0,0).to_s}
+        Setting.data.reload
+        assert{Setting.start_at == Time.local(2015,1,1,10,0,0)}
+      end
+      it "start" do
+        Setting.data.update! start: "10:00"
+        assert{Setting.find_by(key: "start").value == Time.local(2000,1,1,10,0,0).to_s}
+        Setting.data.reload
+        assert{Setting.start == Time.local(2000,1,1,10,0,0)}
       end
     end
     describe "空文字列での設定とキャスト、永続化からの復元" do
